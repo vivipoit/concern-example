06a1b9a19c0f70bb095c4c2403eca50877255358
Vivi Poit <vwpoit@gmail.com>
2020-06-02 10:23:39 +0630

    Create test for Invoice creation

---
+++ spec/features/user_creates_invoice_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature 'user creates invoice' do
+  scenario 'sucessfully' do
+    visit invoices_path
+    click_on 'New Invoice'
+    fill_in 'Subtotal', with: 1000
+    fill_in 'Discount', with: 200
+    fill_in 'Total', with: 800
+    click_on 'Create Invoice'
+
+    expect(page).to have_content 'Invoice was successfully created.'
+    expect(page).to have_content 'Subtotal: 1000'
+    expect(page).to have_content 'Discount: 200'
+    expect(page).to have_content 'Total: 800'
+  end
+end
