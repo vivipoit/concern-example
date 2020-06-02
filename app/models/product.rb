9d3836b2a4d72d9e963addfb9cfa5eb954bc1b86
Vivi Poit <vwpoit@gmail.com>
2020-06-02 12:45:49 +0630

    Use Discounted in Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,5 @@
 class Product < ApplicationRecord
-  before_save :set_sale_price
-
-  def set_sale_price
-    self.regular_price ||= 0
-    self.discount ||= 0
-    self.sale_price = regular_price - discount
-  end
+  include Discounted
+  alias_attribute :original_amount, :regular_price
+  alias_attribute :final_amount, :sale_price
 end
---
+++ spec/models/product_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+describe Product do
+  it_behaves_like 'discounted'
+end
