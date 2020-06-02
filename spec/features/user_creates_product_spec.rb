cf2acd63f257501b2e9aaebd3e5bafc64a7818aa
Vivi Poit <vwpoit@gmail.com>
2020-06-02 10:39:57 +0630

    Create test for Product creation

---
+++ spec/features/user_creates_product_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature 'user creates product' do
+  scenario 'sucessfully' do
+    visit products_path
+    click_on 'New Product'
+    fill_in 'Regular price', with: 1000
+    fill_in 'Discount', with: 200
+    fill_in 'Sale price', with: 800
+    click_on 'Create Product'
+
+    expect(page).to have_content 'Product was successfully created.'
+    expect(page).to have_content 'Regular price: 1000'
+    expect(page).to have_content 'Discount: 200'
+    expect(page).to have_content 'Sale price: 800'
+  end
+end
